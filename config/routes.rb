66302026b05f57c3d873b23a3477e43849f7009d
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-12-20 16:56:12 +0200

    routes and seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'stations/show'
 
+  resources :stations do
+    collection do
+      get ':major/:minor' => 'stations#show'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Store.create(
+							name: "Bubby's", 
+							address: "Bubby's High Line
+71 Gansevoort Street
+New York, NY 10014
+United States",
+							phone_number: "212-206-6200",
+							hours: "Monday	6:00 am – 4:00 pm
+Tuesday	6:00 am – 4:00 pm
+Wednesday	6:00 am – 4:00 pm
+Thursday	6:00 am – 4:00 pm
+Friday	6:00 am – 4:00 pm
+Saturday	6:00 am – 4:00 pm
+Sunday	6:00 am – 4:00 pm",
+							site_url: "http://www.bubbys.com/our-restaurants/high-line/",
+							menu_url: "http://www.bubbys.com/menus/HL_lunch.pdf",
+							photo_url: "http://www.bubbys.com/wp-content/uploads/2013/10/Bubbys-Soda-Fountain-570x427.jpg",
+							major: 1
+)
+
+Station.create(major: 1, minor: 0)
